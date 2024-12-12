a6eb25848d578f6c5528aa800b99d59c76c56154
zzf <mdzxzhao@163.com>
2024-12-12 16:54:46 +0800

    commit 实现斜着走

--- src/main/java/com/zzf/tank/TankFrame.java
+++ src/main/java/com/zzf/tank/TankFrame.java
@@ -8,20 +8,20 @@ public class TankFrame extends Frame {
     int x = 200;
     int y = 200;
 
-    public TankFrame(){
-        //大小  长和宽
-        setSize(800,600);
-        //能否改变大小
+    public TankFrame() {
+        // 大小  长和宽
+        setSize(800, 600);
+        // 能否改变大小
         setResizable(false);
 
         setVisible(true);
-        //标题
+        // 标题
         setTitle("tank war");
 
-        //添加 键盘监听事件
+        // 添加 键盘监听事件
         this.addKeyListener(new MyKeyListener());
 
-        //关闭窗口
+        // 关闭窗口
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
@@ -32,20 +32,19 @@ public class TankFrame extends Frame {
 
     /**
      * 会自动调用  在窗口需要重新绘制的时候
+     *
      * @param g 一支画笔
      */
     @Override
     public void paint(Graphics g) {
 
         // 版本2 填充一个矩形
-        //g.fillRect(200, 200, 50, 50);
+        // g.fillRect(200, 200, 50, 50);
 
-        //版本3 想让矩形动起来 win + d 算一次重新绘制， 可以看到效果
+        // 版本3 想让矩形动起来 win + d 算一次重新绘制， 可以看到效果
         g.fillRect(x, y, 50, 50);
-        x += 30;
+        // x += 30;
         // y += 30;
-
-
 
 
     }
@@ -55,19 +54,81 @@ public class TankFrame extends Frame {
      */
     class MyKeyListener extends KeyAdapter {
 
-        //键盘按下调用
+        // 用于判断方向， 实现同时按左上， 斜着走的功能
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+
+        // 键盘按下调用
         @Override
         public void keyPressed(KeyEvent e) {
             // x += 200;
-            //会默认调用 paint 方法
+            // 会默认调用 paint 方法
             // repaint();
+
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+
+                default:
+                    break;
+            }
+
+            if (bL) {
+                x -= 10;
+            }
+            if (bU) {
+                y -= 10;
+            }
+            if (bR) {
+                x += 10;
+            }
+            if (bD) {
+                y += 10;
+            }
 
         }
 
-        //键盘抬起调用
+        // 键盘抬起调用
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key released");
+
+            int keyCode = e.getKeyCode();
+            switch (keyCode) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
