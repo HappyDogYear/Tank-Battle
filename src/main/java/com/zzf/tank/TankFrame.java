f420c59b0265f49252228fb049fdb178de66a726
zzf <mdzxzhao@163.com>
2024-12-18 14:31:19 +0800

    commit 图片旋转，美化

--- src/main/java/com/zzf/tank/TankFrame.java
+++ src/main/java/com/zzf/tank/TankFrame.java
@@ -16,8 +16,8 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 public class TankFrame extends Frame {
 
-    public static final int GAME_WIDTH = 800;
-    public static final int GAME_HEIGHT = 600;
+    public static final int GAME_WIDTH = 1080;
+    public static final int GAME_HEIGHT = 960;
 
     Tank mainTank = new Tank(200, 400, DirectionEnums.DOWN, this, GroupEnums.GOOD);
 
--- src/main/java/com/zzf/utils/ImageUtils.java
+++ src/main/java/com/zzf/utils/ImageUtils.java
@@ -1,6 +1,7 @@
 package com.zzf.utils;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
@@ -19,16 +20,15 @@ public class ImageUtils {
     public static BufferedImage[] explodes = new BufferedImage[16];
     static {
         try {
-            tankL = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
-            tankU = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
-            tankR = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
-            tankD = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+            tankU = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/BadTank1.png"));
+            tankL = rotateImage(tankU, -90);
+            tankR = rotateImage(tankU, 90);
+            tankD = rotateImage(tankU, 180);
 
-
-            bulletL = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/bulletL.gif"));
             bulletU = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/bulletU.gif"));
-            bulletR = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/bulletR.gif"));
-            bulletD = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/bulletD.gif"));
+            bulletL = rotateImage(bulletU, -90);
+            bulletR = rotateImage(bulletU, 90);
+            bulletD = rotateImage(bulletU, 180);
 
             for (int i = 0; i < 16; i++) {
                 explodes[i] = ImageIO.read(ImageUtils.class.getClassLoader().getResourceAsStream("images/e" + (i+1) + ".gif"));
@@ -36,5 +36,28 @@ public class ImageUtils {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+
+    /**
+     * 图片旋转
+     * @param bufferedimage
+     * @param degree
+     * @return
+     */
+    public static BufferedImage rotateImage(BufferedImage bufferedimage, int degree) {
+        int w = bufferedimage.getWidth();
+        int h = bufferedimage.getHeight();
+        int type = bufferedimage.getColorModel().getTransparency();
+        BufferedImage img;
+        Graphics2D graphics2d;
+        (graphics2d = (img = new BufferedImage(w, h, type))
+                .createGraphics()).setRenderingHint(
+                RenderingHints.KEY_INTERPOLATION,
+                RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        graphics2d.rotate(Math.toRadians(degree), w / 2, h / 2);
+        graphics2d.drawImage(bufferedimage, 0, 0, null);
+        graphics2d.dispose();
+        return img;
     }
 }
